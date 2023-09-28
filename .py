70c8af1396b3138e14aa25f970e1f8693c3b9b52
shinchann <140788776+kavya5c0@users.noreply.github.com>
2023-09-28 13:05:23 +0530

    Create .py
    Here's what this code does:
    
    It defines four functions (add, subtract, multiply, and divide) to perform the basic arithmetic operations.
    
    It presents a menu of operations to the user, allowing them to select which operation they want to perform.
    
    It checks if the user's choice is valid (1, 2, 3, or 4). If not, it displays an error message.
    
    If the user's choice is valid, it takes two numeric inputs from the user and performs the selected operation, displaying the result.
    
    It includes a check to prevent division by zero, displaying an error message if the user attempts to divide by zero.
    
    This simple calculator can handle basic arithmetic operations and provides user-friendly input validation. Users can choose the operation they want to perform, input numbers, and get the result.

---
+++ .py
@@ -0,0 +1,49 @@
+# Simple Calculator in Python
+
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+# Main program
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+# Take input from the user
+choice = input("Enter choice (1/2/3/4): ")
+
+# Check if the input is a valid choice
+if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        result = divide(num1, num2)
+        if result == "Cannot divide by zero!":
+            print("Error:", result)
+        else:
+            print("Result:", result)
+else:
+    print("Invalid input. Please enter a valid choice (1/2/3/4).")
